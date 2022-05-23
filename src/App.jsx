a1bce620e19b34cbaa8c19f05ea54f29a5a03545
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-23 11:33:09 +0200

    App.jsx finished. pages added..

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.scss";
+import { PageWelcome } from "./pages/PageWelcome";
+import { PageBooks } from "./pages/PageBooks";
+import { NavLink, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">Hello!</div>;
+  return (
+    <div className="App">
+      <h1>React Site</h1>
+      <NavLink to="/welcome">Welcome</NavLink> |{" "}
+      <NavLink to="/books">Books</NavLink>
+      <Routes>
+        <Route path="/welcome" element={<PageWelcome />} />
+        <Route path="/books" element={<PageBooks />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
